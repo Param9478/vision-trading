d5fe8de22e29be295242e0700ffb28d9a86f1131
param9478 <singh.parminder9478@gmail.com>
2025-02-08 11:26:22 -0700

    removing bots, about and contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 // App.jsx
 import { Routes, Route } from 'react-router-dom';
-// import { Home, Charts, Bots, About, Contact } from './pages';
+
 import Home from './pages/Home';
 import Charts from './pages/Charts';
-import Bots from './pages/Bots';
-import About from './pages/About';
-import Contact from './pages/Contact';
-// import { Header, Footer } from './components';
+// import Bots from './pages/Bots';
+// import About from './pages/About';
+// import Contact from './pages/Contact';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -17,9 +17,9 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/charts" element={<Charts />} />
-        <Route path="/bots" element={<Bots />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        {/* <Route path="/bots" element={<Bots />} /> */}
+        {/* <Route path="/about" element={<About />} /> */}
+        {/* <Route path="/contact" element={<Contact />} /> */}
       </Routes>
       <Footer />
     </div>
